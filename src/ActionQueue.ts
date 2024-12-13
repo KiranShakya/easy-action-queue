37f7ee59bf89a08b172a49bedef0eac66be927fc
KiranShakya <kiranshakya.id@gmail.com>
2024-12-13 10:59:20 +0545

    Update ActionQueue.ts
    Improved generics and typings

--- src/ActionQueue.ts
+++ src/ActionQueue.ts
@@ -4,8 +4,8 @@ interface QueueItem<T> {
   action: () => T;
   resolve: (result: T) => void;
 }
-export default class EasyActionQueue<T> {
-  private readonly queue: Array<QueueItem<T>> = [];
+export default class EasyActionQueue {
+  private readonly queue: Array<QueueItem<unknown>> = [];
   private running: number = 0;
 
   constructor(private readonly concurrency: number = 1) {
@@ -15,30 +15,36 @@ export default class EasyActionQueue<T> {
     }
   }
 
-  async enqueue(action: () => T): Promise<T> {
+  /**
+  * T is type for final value, R is type of return value from the callback
+  * action callback will be enqueued and when it gets triggered, the promise will resolve with the result
+  * if the callback returns a string, it will be resolved in promise
+  * if it is async operation, the result will be resolved in promise
+  */
+  async enqueue<T, R>(action: () => R): Promise<T> {
     return new Promise<T>((resolve) => {
       this.queue.push({ action, resolve });
-      this.processQueue();
+      this.processQueue<T, R>();
     });
   }
 
-  private async processQueue(): Promise<void> {
+  private async processQueue<T, R>(): Promise<void> {
     while (this.running < this.concurrency && this.queue.length > 0) {
-      const item = this.queue.shift()!;
+      const item = this.queue.shift() as QueueItem<R>;
       this.running++;
-      const response: unknown = item.action();
+      const response: unknown = item.action() as R;
       if (response instanceof Promise) {
-        response.then(result => this.handleResponse(result, item));
+        response.then((result: R) => this.handleResponse<T, R>(result, item));
       } else if (response instanceof Observable) {
-        firstValueFrom(response).then(result => this.handleResponse(result, item))
+        firstValueFrom(response).then((result: R) => this.handleResponse<T, R>(result, item));
       } else {
-        this.handleResponse(response as T, item);
+        this.handleResponse<T, R>(response, item);
       }
     }
   }
-  private handleResponse(result: T, item: QueueItem<T>) {
-        this.running--;
-        item.resolve(result);
-        this.processQueue();
+  private handleResponse<T, R>(result: R, item: QueueItem<R>) {
+    this.running--;
+    item.resolve(result);
+    this.processQueue<T, R>();
   }
 }
